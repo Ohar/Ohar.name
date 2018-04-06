78d8eff1f299b428b365c74dbb266f95b04de427
Pavel Lysenko <code@ohar.name>
2018-04-06 23:56:23 +0200

    Quotes: fix empty lines

--- old_static_site/quotes/js/init.js
+++ old_static_site/quotes/js/init.js
@@ -1,13 +1,13 @@
 $(function() {
     window.quotes = [];
-    
-    $('.quotes_dump').load('quotes.txt', function () { // Грузим файл	
-        var quotes = $('.quotes_dump').html().split('\n'); // Парсим построчно
-            
+
+    $('.quotes_dump').load('quotes.txt', function () { // Грузим файл
+        var quotes = $('.quotes_dump').html().trim().split('\n'); // Парсим построчно
+
         for (i in quotes) {
             var quote_arr = quotes[i].split('©'),
                 quote_origin_arr = quote_arr[1].split('@');
-                
+
             window.quotes.push({
                 text: quote_arr[0].trim(),
                 origin: {

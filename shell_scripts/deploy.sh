9a8fd5b4012e0c1034cc93e5eea55a3981fff1a0
Pavel Lysenko <code@ohar.name>
2018-04-06 22:26:06 +0200

    Add deploy script

---
+++ shell_scripts/deploy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Save script directory path
+# https://stackoverflow.com/a/24112741/2059884
+declare -r PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$PARENT_PATH"
+
+declare -r FOLDER=`node -p 'require("./../config.json")["path"]'`;
+declare -r SERVER=`node -p 'require("./../config.json")["server"]'`;
+declare -r USERNAME=`node -p 'require("./../config.json")["username"]'`;
+declare -r URL=$USERNAME@$SERVER
+declare -r FILE_NAME="old_static_site.zip"
+
+scp ./../dist/$FILE_NAME $URL:~
+
+ssh -tt $URL <<DEPLOY
+    echo $FOLDER
+    echo $USERNAME
+    echo $SERVER
+    sudo rm -rf $FOLDER/*
+    sudo mv ~/$FILE_NAME $FOLDER
+    cd $FOLDER
+    sudo unzip -u $FILE_NAME
+    sudo rm $FILE_NAME
+    sudo systemctl restart nginx
+    echo "Done"
+    exit
+DEPLOY

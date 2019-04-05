5cb4aeae8b49cbc705568c5a9f8c92996d487989
Pavel Lysenko <code@ohar.name>
2019-04-06 02:13:26 +0300

    Layout: update footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import YandexMetrika from '@/components/YandexMetrika'
+
+const Footer = () => (
+  <footer className="Footer">
+    <YandexMetrika/>
+  </footer>
+)
+
+export default Footer
--- src/components/Layout/layout.jsx
+++ src/components/Layout/layout.jsx
@@ -1,15 +1,10 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
 import React from 'react'
 import PropTypes from 'prop-types'
-import { StaticQuery, graphql } from 'gatsby'
+import { StaticQuery, graphql } from "gatsby"
 
 import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+
 import './layout.css'
 
 const Layout = ({ children }) => (
@@ -35,11 +30,7 @@ const Layout = ({ children }) => (
           }}
         >
           <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href='https://www.gatsbyjs.org'>Gatsby</a>
-          </footer>
+          <Footer/>
         </div>
       </>
     )}

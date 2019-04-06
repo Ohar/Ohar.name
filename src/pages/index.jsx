b5b784b068bddb740ca070170c99c3e25187e809
Pavel Lysenko <code@ohar.name>
2019-04-07 00:18:01 +0300

    Add Gatsby

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,20 +1,31 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
 import Layout from '@/components/Layout'
-import Image from '@/components/Image'
 import SEO from '@/components/Seo'
+
+import ImgDeus from '@/images/deus.png'
+import ImgEst from '@/images/est.png'
+import ImgMachina from '@/images/machina.png'
 
 const IndexPage = () => (
   <Layout>
     <SEO title='Home' keywords={['Pavel Lysenko site', 'Сайт Павла Лысенко', 'Ohar']} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to='/page-2/'>Go to page 2</Link>
+    <h1 className="offset">Сайт Павла Лысенко</h1>
+
+    <article className="mechanicus">
+      <h1 className="offset">Deus Mechanicus</h1>
+
+      <section className="mechanicus__logo">
+        <h1 className="offset">Omnissian</h1>
+      </section>
+
+      <section className="mechanicus__motto">
+        <h1 className="offset">Deus est machina</h1>
+        <img src={ImgDeus} width={229} height={85}/>
+        <img src={ImgEst} width={173} height={85}/>
+        <img src={ImgMachina} width={410} height={85}/>
+      </section>
+    </article>
   </Layout>
 )
 

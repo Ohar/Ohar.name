51ecd1e0588bab4ad1eed06be03764c92cc8f3a1
Pavel Lysenko <code@ohar.name>
2019-04-07 00:18:01 +0300

    Add Gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`,
       },
     },
     {
@@ -44,9 +44,6 @@ module.exports = {
           'jsx',
         ]
       }
-    }
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    },
   ],
 }
---
+++ src/components/YandexMetrika.jsx
@@ -0,0 +1,46 @@
+import React from "react"
+
+const YandexMetrika = () => (
+  <section
+    dangerouslySetInnerHTML={{
+      __html: `
+        <div> 
+            <script type="text/javascript">
+              ((d,w,c) => {
+                (w[c] = w[c] || []).push(() => {
+                  try {
+                    w.yaCounter21398491 = new Ya.Metrika({
+                      id: 21398491,
+                      webvisor: true,
+                      clickmap: true,
+                      trackLinks: true,
+                      accurateTrackBounce: true,
+                      trackHash: true,
+                    })
+                  } catch (e) {}
+                })
+        
+                var n = d.getElementsByTagName("script")[0], s = d.createElement("script"),
+                f = function () {n.parentNode.insertBefore(s, n)}
+                s.type = "text/javascript"
+                s.async = true
+                s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js"
+        
+                if (w.opera == "[object Opera]") {
+                  d.addEventListener("DOMContentLoaded", f, false)
+                } else {
+                  f()
+                }
+              })(document,window,'yandex_metrika_callbacks')
+            </script>
+        
+            <noscript>
+              <div><img src="//mc.yandex.ru/watch/21398491" style="position:absolute; left:-9999px;" alt=""/></div>
+            </noscript>
+        </div>
+      `,
+    }}
+  />
+)
+
+export default YandexMetrika

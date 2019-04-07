f6f7a2c42f543e9e709810e53182a7c7f7ce1617
Pavel Lysenko <code@ohar.name>
2019-04-07 05:15:21 +0300

    Add Gatsby

---
+++ src/pages/articles/rules/rules-html-bw.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Layout from '@/components/Layout'
+import Seo from '@/components/Seo'
+
+import rulesRawText from './rules-raw-text'
+
+import './styles/print.css'
+import './styles/print_BW.css'
+
+export default () => (
+  <Layout>
+    <Seo title='Правила оформления текстов на русском языке' />
+    <section>
+      <h1>Правила оформления текстов на русском языке</h1>
+      <article dangerouslySetInnerHTML={{ __html: rulesRawText}}/>
+    </section>
+  </Layout>
+)

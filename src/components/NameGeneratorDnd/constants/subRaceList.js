24edd027b41fc6c058998af178bf080cc1f94c7a
Pavel Lysenko <code@ohar.name>
2019-05-02 03:05:35 +0300

    subRaceList: add ”any” race

--- src/components/NameGeneratorDnd/constants/subRaceList.js
+++ src/components/NameGeneratorDnd/constants/subRaceList.js
@@ -1,16 +1,13 @@
 const subRaceList = [
   {
+    text: 'Любая',
+    value: 'any',
+  },
+  {
     text: 'Чульты',
     value: 'human:chult',
     show: {
       race: 'human',
-    },
-  },
-  {
-    text: 'Табакси',
-    value: 'tabaxi',
-    show: {
-      race: 'tabaxi',
     },
   },
 ]

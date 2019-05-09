d0b1b6b8e9b17a3b1ba72f1a4b4adb5119e84581
Pavel Lysenko <code@ohar.name>
2019-05-09 21:11:24 +0300

    enhanceTypeCollectionWithVisibility: set defaults for hidden values

--- src/components/ContentGenerator/utils/enhanceTypeCollectionWithVisibility.js
+++ src/components/ContentGenerator/utils/enhanceTypeCollectionWithVisibility.js
@@ -1,41 +1,53 @@
 const enhanceTypeCollectionWithVisibility = typeCollection => Object
-  .keys(typeCollection)
-  .reduce(
-    (result, type) => {
-      const {show} = typeCollection[type]
-      const isTypeVisible = !(
-        show
-        && Object.keys(show).find(
-          key => typeCollection[key].chosen !== show[key]
-        )
-      )
-
-      const list = typeCollection[type].list.map(
-        ({show: showItem, ...rest}) => {
-          const visible = !(
-            showItem && Object.keys(showItem).find(
-              key => typeCollection[key].chosen !== showItem[key]
+    .keys(typeCollection)
+    .reduce(
+        (result, type) => {
+            const {show, list, chosen} = typeCollection[type]
+            const isTypeVisible = !(
+                show
+                && Object.keys(show).find(
+                    key => typeCollection[key].chosen !== show[key]
+                )
             )
-          )
 
-          return {
-            ...rest,
-            show: showItem,
-            visible,
-          }
-        }
-      )
+            const [{value: chosenDefault}] = list
 
-      return {
-        ...result,
-        [type]: {
-          ...typeCollection[type],
-          visible: isTypeVisible,
-          list,
+            let updatedChosen = isTypeVisible
+                ? chosen
+                : chosenDefault
+
+            const updatedList = list.map(
+                ({show: showItem, value, ...rest}) => {
+                    const visible = !(
+                        showItem && Object.keys(showItem).find(
+                            key => typeCollection[key].chosen !== showItem[key]
+                        )
+                    )
+
+                    if (isTypeVisible && !visible && chosen === value) {
+                        updatedChosen = chosenDefault
+                    }
+
+                    return {
+                        ...rest,
+                        show: showItem,
+                        value,
+                        visible,
+                    }
+                }
+            )
+
+            return {
+                ...result,
+                [type]: {
+                    ...typeCollection[type],
+                    chosen: updatedChosen,
+                    list: updatedList,
+                    visible: isTypeVisible,
+                },
+            }
         },
-      }
-    },
-    {}
-  )
+        {}
+    )
 
 export default enhanceTypeCollectionWithVisibility

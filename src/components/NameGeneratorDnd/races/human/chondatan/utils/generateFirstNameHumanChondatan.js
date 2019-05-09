8e1ed60561ed1b6495a8ef1ec52a5bca1d5fa7cb
Pavel Lysenko <code@ohar.name>
2019-05-09 14:21:28 +0300

    Dnd name generator: add “chondatan”

--- src/components/NameGeneratorDnd/races/human/chondatan/utils/generateFirstNameHumanChondatan.js
+++ src/components/NameGeneratorDnd/races/human/chondatan/utils/generateFirstNameHumanChondatan.js
@@ -2,10 +2,10 @@ import _ from "lodash"
 
 import generateNameListHumanChondatan from "./generateNameListHumanChondatan"
 
-const generateFirstNameHumanHondatan = ({gender, type}) => {
+const generateFirstNameHumanChondatan = ({gender, type}) => {
   const nameList = generateNameListHumanChondatan(gender, type)
 
   return _.sample(nameList)
 }
 
-export default generateFirstNameHumanHondatan
+export default generateFirstNameHumanChondatan

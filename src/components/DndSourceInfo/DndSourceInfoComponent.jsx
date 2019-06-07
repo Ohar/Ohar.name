ddd93937297f49c979d1a4e9e2e137338e9fc81f
Pavel Lysenko <lysenkopa@msk.bcs.ru>
2019-06-07 18:43:44 +0300

    DndSourceInfoComponent: remove console

--- src/components/DndSourceInfo/DndSourceInfoComponent.jsx
+++ src/components/DndSourceInfo/DndSourceInfoComponent.jsx
@@ -5,12 +5,8 @@ import {dndManualCollection} from "@/constants/dnd/dndManualList"
 import './DndSourceInfoStyles.css'
 
 const DndSourceInfoComponent = ({ source }) => {
-
-    console.log('DndSourceInfoComponent', source);
     const [sourceName, sourcePage] = source.split(':')
     const sourceData = dndManualCollection[sourceName]
-
-    console.log('DndSourceInfoComponent', source, sourceName, sourcePage);
 
     return sourceData ? (
       <section className='DndSourceInfo'>

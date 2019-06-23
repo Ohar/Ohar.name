b948ab140428a24c4ec418a8f0a9c6ed09d4ab76
Pavel Lysenko <code@ohar.name>
2019-06-23 12:21:24 +0300

    SentientItem: specify god

--- src/components/DndSentientItemGenerator/SentientItem.js
+++ src/components/DndSentientItemGenerator/SentientItem.js
@@ -1,8 +1,11 @@
+import {dndGodCollection} from "@/constants/dnd/dndGodList"
+
 import aligmentList from "./constants/aligmentList"
 import charBadList from "./constants/charBadList"
 import charGoodList from "./constants/charGoodList"
 import interactionTypeList from "./constants/interactionTypeList"
 import goalList from "./constants/goalList"
+import godList from "./constants/godList"
 import senseTypeList from "./constants/senseTypeList"
 import mannerList from "./constants/mannerList"
 import communicationList from "./constants/communicationList"
@@ -34,9 +37,36 @@ export default class SentientItem {
 
     this.aligmentId = pickByPropability(aligmentList).id
     this.interactions = pickByPropability(interactionTypeList).description
-    this.goal = pickByPropability(goalList).description
+    this.goal = this.generateGoal()
     this.senses = pickByPropability(senseTypeList).description
     this.manner = pickByPropability(mannerList).description
     this.communication = pickByPropability(communicationList).description
+  }
+
+  generateGoal = () => {
+    const goal = pickByPropability(goalList)
+
+    switch (goal.id) {
+      // case 'aligment': {
+      //
+      // }
+      //
+      // case 'defender':
+      // case 'doom': {
+      //
+      // }
+
+      case 'crusader':
+      case 'tamplier': {
+        const godId = pickByPropability(godList).id
+        const god = dndGodCollection[godId]
+
+        return goal.description.replace('определённого божества', god.name.full.genitive)
+      }
+
+      default: {
+        return goal.description
+      }
+    }
   }
 }

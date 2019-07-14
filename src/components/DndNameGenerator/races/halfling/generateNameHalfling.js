98933a14e590b687f53c073b2600efb02edd9f65
Pavel Lysenko <code@ohar.name>
2019-07-14 14:35:33 +0300

    DndNameGenerator: generate family names for halflings

--- src/components/DndNameGenerator/races/halfling/generateNameHalfling.js
+++ src/components/DndNameGenerator/races/halfling/generateNameHalfling.js
@@ -1,14 +1,14 @@
 import _ from "lodash";
 
 import generateNameListHalfling from "./utils/generateNameListHalfling"
-import familyNameList from "./dict/names_family"
+import generateFamilyNameHalfling from "./utils/generateFamilyNameHalfling"
 
 export default ({gender}) => {
   const genderToUse = (gender === 'male' || gender === 'female')
     ? gender
     : _.sample(['male', 'female'])
   const name = _.sample(generateNameListHalfling(genderToUse))
-  const familyName = _.sample(familyNameList)[genderToUse]
+  const familyName = generateFamilyNameHalfling(genderToUse)
 
   return `${name} ${familyName}`
 }

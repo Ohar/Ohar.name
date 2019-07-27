310c0b3a56d4ddaf6d4e466342920f9ed00eb7d6
Pavel Lysenko <code@ohar.name>
2019-07-27 23:36:06 +0300

    Tabaxi schemaList: simplify code

--- src/components/DndNameGenerator/races/tabaxi/constants/schemaList.js
+++ src/components/DndNameGenerator/races/tabaxi/constants/schemaList.js
@@ -12,73 +12,50 @@ const schemaList = [
   {
     text: 'Любая',
     value: 'any',
-    show: {
-      race: 'tabaxi',
-    },
   },
   {
     text: 'Какое-то что-то',
     value: ADJECTIVE_NOUN,
-    show: {
-      race: 'tabaxi',
-    },
   },
   {
     text: 'Сколько-то какого-то чего-то',
     value: NUMBER_ADJECTIVE_NOUN,
-    show: {
-      race: 'tabaxi',
-    },
   },
   {
     text: 'Что-то на чём-то',
     value: NOUN_AT_NOUN,
-    show: {
-      race: 'tabaxi',
-    },
   },
   {
     text: 'Что-то чего-то',
     value: NOUN_OF_NOUN,
-    show: {
-      race: 'tabaxi',
-    },
   },
   {
     text: 'Что-то скольки-то чего-то',
     value: NOUN_OF_NUMBER_NOUN,
-    show: {
-      race: 'tabaxi',
-    },
   },
   {
     text: 'Сколько-то чего-то чего-то',
     value: NUMBER_NOUN_OF_NOUN,
-    show: {
-      race: 'tabaxi',
-    },
   },
   {
     text: 'Что-то какого-то чего-то',
     value: NOUN_OF_ADJECTIVE_NOUN,
-    show: {
-      race: 'tabaxi',
-    },
   },
   {
     text: 'Что-то скольки-то какого-то чего-то',
     value: NOUN_OF_NUMBER_ADJECTIVE_NOUN,
-    show: {
-      race: 'tabaxi',
-    },
   },
   {
     text: 'Сколько-то чего-то какого-то чего-то',
     value: NUMBER_NOUN_OF_ADJECTIVE_NOUN,
-    show: {
-      race: 'tabaxi',
-    },
   },
 ]
 
-export default schemaList
+export default schemaList.map(
+  item => ({
+    ...item,
+    show: {
+      race: 'tabaxi',
+    },
+  })
+)

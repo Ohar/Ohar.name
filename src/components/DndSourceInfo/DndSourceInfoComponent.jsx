480766490350150878d9b95bae54abaef0327b7e
Pavel Lysenko <code@ohar.name>
2019-07-31 04:43:07 +0300

    DndSource: use short names

--- src/components/DndSourceInfo/DndSourceInfoComponent.jsx
+++ src/components/DndSourceInfo/DndSourceInfoComponent.jsx
@@ -1,23 +1,32 @@
 import React from 'react'
 
-import {dndManualCollection} from "@/constants/dnd/dndManualList"
+import { dndManualCollection } from '@/constants/dnd/dndManualList'
 
 import './DndSourceInfoStyles.css'
 
 const DndSourceInfoComponent = ({ source }) => {
-    const [sourceName, sourcePage] = source.split(':')
-    const sourceData = dndManualCollection[sourceName]
+  const [sourceName, sourcePage] = source.split(':')
+  const sourceData = dndManualCollection[sourceName]
 
-    return sourceData ? (
-      <section className='DndSourceInfo'>
-          {sourceData.name}
-          {
-              sourcePage
-                ? `, ${sourcePage}`
-                : ''
-          }
+  if (sourceData) {
+    const pageText = sourcePage
+      ? `, ${sourcePage}`
+      : ''
+
+    const title = `${sourceData.name}${pageText}`
+    const text = `${sourceData.shortName}${pageText}`
+
+    return (
+      <section
+        className='DndSourceInfo'
+        title={title}
+      >
+        {text}
       </section>
-    ) : null
+    )
+  }
+
+  return null
 }
 
 export default DndSourceInfoComponent
--- src/constants/dnd/dndManualList.js
+++ src/constants/dnd/dndManualList.js
@@ -4,22 +4,27 @@ const dndManualList = [
   {
     id: 'PHB',
     name: 'Книга игрока',
+    shortName: 'КИ',
   },
   {
     id: 'DMG',
     name: 'Руководство мастера',
+    shortName: 'РМ',
   },
   {
     id: 'MM',
     name: 'Бестиарий',
+    shortName: 'Б',
   },
   {
     id: 'VGtM',
     name: 'Руководство Воло по монстрам',
+    shortName: 'РВпМ',
   },
   {
     id: 'ToA',
     name: 'Гробница Аннигиляции',
+    shortName: 'ГА',
   },
 ]
 

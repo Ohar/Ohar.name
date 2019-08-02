199d81d3519c9adb222701528070dc1ceb8bef6f
Pavel Lysenko <code@ohar.name>
2019-08-03 00:07:04 +0300

    DndSentientItem: use array to form table

--- src/components/DndSentientItemGenerator/components/DndSentientItem/DndSentientItemComponent.jsx
+++ src/components/DndSentientItemGenerator/components/DndSentientItem/DndSentientItemComponent.jsx
@@ -1,130 +1,33 @@
 import React from "react"
 import ReactMarkdown from "react-markdown"
 
-import {dndParamCollection} from "@/constants/dnd/dndParamList"
-import {dndAligmentCollection} from "@/constants/dnd/dndAligmentList"
-import {dndItemCollection} from "@/constants/dnd/dndItemList"
-
 import "./DndSentientItemStyles.css"
 
-const DndSentientItem = (
-  {
-    item: {
-      aligmentId,
-      bond,
-      char,
-      communication,
-      creator,
-      goal,
-      ideal,
-      interactions,
-      manner,
-      name,
-      senses,
-      stats,
-      type,
-      weakness,
-    }
-  }
-) => {
-  const {
-    name: typeName,
-    description: typeDescription,
-    category: typeCategory,
-    subcategory,
-  } = dndItemCollection[type]
-
-  const typeSubcategory = subcategory
-    ? ` — ${subcategory}`
-    : ''
-
-  return (
-    <table className='DndSentientItem'>
+const DndSentientItemComponent = ({itemDataList}) => (
+  <table className='DndSentientItem'>
     <tbody>
-      <tr className='DndSentientItem_row'>
-        <th className='DndSentientItem_cell DndSentientItem_cell-head'>Название</th>
-        <td className='DndSentientItem_cell'>{name}</td>
-      </tr>
-      <tr className='DndSentientItem_row'>
-        <th className='DndSentientItem_cell DndSentientItem_cell-head'>Тип предмета</th>
-        <td
-          className='DndSentientItem_cell'
-          title={`${typeCategory} ${typeSubcategory}\n${typeDescription}`}
-        >
-          {typeName}
-        </td>
-      </tr>
-      <tr className='DndSentientItem_row'>
-        <th className='DndSentientItem_cell DndSentientItem_cell-head'>Параметры</th>
-        <td className='DndSentientItem_cell'>
-          <ul>
-            {
-              stats.map(
-                ({id, value}) => (
-                  <li key={id}>
-                    {dndParamCollection[id].shortName} {value}
-                  </li>
-                )
-              )
-            }
-          </ul>
-        </td>
-      </tr>
-      <tr className='DndSentientItem_row'>
-        <th className='DndSentientItem_cell DndSentientItem_cell-head'>Черты характера</th>
-        <td className='DndSentientItem_cell DndSentientItem_cell-charText'>
-          {char.good}, но {char.bad}.
-        </td>
-      </tr>
-      <tr className='DndSentientItem_row'>
-        <th className='DndSentientItem_cell DndSentientItem_cell-head'>Мировоззрение</th>
-        <td className='DndSentientItem_cell DndSentientItem_cell-aligment'>{dndAligmentCollection[aligmentId].name.nominative}</td>
-      </tr>
-      <tr className='DndSentientItem_row'>
-        <th className='DndSentientItem_cell DndSentientItem_cell-head'>Чувства</th>
-        <td className='DndSentientItem_cell'>{senses}</td>
-      </tr>
-      <tr className='DndSentientItem_row'>
-        <th className='DndSentientItem_cell DndSentientItem_cell-head'>Общение</th>
-        <td className='DndSentientItem_cell'>
-          <ReactMarkdown>{interactions.description}</ReactMarkdown>
-        </td>
-      </tr>
-      <tr className='DndSentientItem_row'>
-        <th className='DndSentientItem_cell DndSentientItem_cell-head'>Предназначение</th>
-        <td className='DndSentientItem_cell'>
-          <ReactMarkdown>{goal}</ReactMarkdown>
-        </td>
-      </tr>
-      <tr className='DndSentientItem_row'>
-        <th className='DndSentientItem_cell DndSentientItem_cell-head'>Манеры</th>
-        <td className='DndSentientItem_cell'>{manner}</td>
-      </tr>
-      <tr className='DndSentientItem_row'>
-        <th className='DndSentientItem_cell DndSentientItem_cell-head'>Идеал</th>
-        <td className='DndSentientItem_cell DndSentientItem_cell-idealText'>{ideal}</td>
-      </tr>
-      <tr className='DndSentientItem_row'>
-        <th className='DndSentientItem_cell DndSentientItem_cell-head'>Особенности</th>
-        <td className='DndSentientItem_cell'>{communication}</td>
-      </tr>
-      <tr className='DndSentientItem_row'>
-        <th className='DndSentientItem_cell DndSentientItem_cell-head'>Привязанность</th>
-        <td className='DndSentientItem_cell'>{bond}</td>
-      </tr>
-      <tr className='DndSentientItem_row'>
-        <th className='DndSentientItem_cell DndSentientItem_cell-head'>Слабость или тайна</th>
-        <td className='DndSentientItem_cell'>{weakness}</td>
-      </tr>
-      <tr className='DndSentientItem_row'>
-        <th className='DndSentientItem_cell DndSentientItem_cell-head'>Создатель</th>
-        <td className='DndSentientItem_cell'>
-          <ReactMarkdown>{creator}</ReactMarkdown>
-        </td>
-      </tr>
+    {
+      itemDataList.map(
+        ({title, header, content, i}) => (
+          <tr
+            className='DndSentientItem_row'
+            key={i}
+          >
+            <th className='DndSentientItem_cell DndSentientItem_cell-head'>
+              {header}
+            </th>
+            <td
+              className='DndSentientItem_cell'
+              title={title || null}
+            >
+              <ReactMarkdown>{content}</ReactMarkdown>
+            </td>
+          </tr>
+        )
+      )
+    }
     </tbody>
-    </table>
-  )
-}
+  </table>
+)
 
-export default DndSentientItem
+export default DndSentientItemComponent
--- src/components/DndSentientItemGenerator/components/DndSentientItem/DndSentientItemContainer.jsx
+++ src/components/DndSentientItemGenerator/components/DndSentientItem/DndSentientItemContainer.jsx
@@ -1,9 +1,137 @@
-import React from "react"
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
-import DndSentientItemComponent from "./DndSentientItemComponent"
+import { dndItemCollection } from '@/constants/dnd/dndItemList'
+import { dndParamCollection } from '@/constants/dnd/dndParamList'
 
-const DndSentientItemContainer = ({item}) => item
-  ? <DndSentientItemComponent item={item}/>
-  : null
+import DndSentientItemComponent from './DndSentientItemComponent'
+import { dndAligmentCollection } from '../../../../constants/dnd/dndAligmentList'
 
-export default DndSentientItemContainer
+export default class DndSentientItemContainer extends Component {
+  static propTypes = {
+    item: PropTypes.shape({
+      aligmentId: PropTypes.string,
+      bond: PropTypes.string,
+      char: PropTypes.string,
+      communication: PropTypes.string,
+      creator: PropTypes.string,
+      goal: PropTypes.string,
+      ideal: PropTypes.string,
+      interactions: PropTypes.string,
+      manner: PropTypes.string,
+      name: PropTypes.string,
+      senses: PropTypes.string,
+      stats: PropTypes.string,
+      type: PropTypes.string,
+      weakness: PropTypes.string,
+    }),
+  }
+
+  static defaultProps = {
+    item: null,
+  }
+
+  render() {
+    const { item } = this.props
+
+    if (item) {
+      const {
+        aligmentId,
+        bond,
+        char,
+        communication,
+        creator,
+        goal,
+        ideal,
+        interactions,
+        manner,
+        name,
+        senses,
+        stats,
+        type,
+        weakness,
+      } = item
+
+      const {
+        name: typeName,
+        description: typeDescription,
+        category: typeCategory,
+        subcategory,
+      } = dndItemCollection[type]
+
+      const typeSubcategory = subcategory
+        ? ` — ${subcategory}`
+        : ''
+
+      const itemDataList = [
+        {
+          header: 'Название',
+          content: name,
+        },
+        {
+          title: `${typeCategory} ${typeSubcategory}\n${typeDescription}`,
+          header: 'Тип предмета',
+          content: typeName,
+        },
+        {
+          header: 'Параметры',
+          content: stats
+            .map(
+              ({ id, value }) => `* ${dndParamCollection[id].shortName} ${value}`,
+            )
+            .join('\n'),
+        },
+        {
+          header: 'Черты характера',
+          content: `${char.good}, но ${char.bad}.`,
+        },
+        {
+          header: 'Мировоззрение',
+          content: dndAligmentCollection[aligmentId].name.nominative,
+        },
+        {
+          header: 'Чувства',
+          content: senses,
+        },
+        {
+          header: 'Общение',
+          content: interactions.description,
+        },
+        {
+          header: 'Предназначение',
+          content: goal,
+        },
+        {
+          header: 'Манеры',
+          content: manner,
+        },
+        {
+          header: 'Идеал',
+          content: ideal,
+        },
+        {
+          header: 'Особенности',
+          content: communication,
+        },
+        {
+          header: 'Привязанность',
+          content: bond,
+        },
+        {
+          header: 'Слабость или тайна',
+          content: weakness,
+        },
+        {
+          header: 'Создатель',
+          content: creator,
+        },
+      ]
+
+      return (
+        <DndSentientItemComponent itemDataList={itemDataList}/>
+      )
+    }
+
+    return null
+  }
+}

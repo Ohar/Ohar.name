7c99fe0db02f99e1197368cd61ca057600257f1d
Pavel Lysenko <code@ohar.name>
2019-08-03 03:35:33 +0300

    SentientItem: separate “pcClassList” from “creatureTypeList”

---
+++ src/components/DndSentientItemGenerator/constants/creatureTypeList.js
@@ -0,0 +1,170 @@
+export default [
+  {
+    id: 'humanoid',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'goblinoid',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'shapeshifter',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'beast',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'fiend',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'celestial',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'monster',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'plant',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'construct',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'undead',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'dragon',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'aberration',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'devil',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'demon',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'kobold',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'titan',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'bullywug',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'gnoll',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'gith',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'gnome',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'grimlock',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'orc',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'grung',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'yugolot',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'dwarf',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'sahuagin',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'elf',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'quaggoth',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'lizardfolk',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'xvart',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'thri_kreen',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'troglodyte',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'giant',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'fire_giant',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'firenewt',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'fey',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'human',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'kuo_toa',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'kenku',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'yoan_ti',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'elemental',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'jelly',
+    probabilityWeight: 1,
+  },
+]
---
+++ src/components/DndSentientItemGenerator/constants/pcClassList.js
@@ -0,0 +1,50 @@
+export default [
+  {
+    id: 'barbarian',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'bard',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'priest',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'druid',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'fighter',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'monk',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'paladin',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'ranger',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'rogue',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'sorcerer',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'warlock',
+    probabilityWeight: 1,
+  },
+  {
+    id: 'wizard',
+    probabilityWeight: 1,
+  },
+]

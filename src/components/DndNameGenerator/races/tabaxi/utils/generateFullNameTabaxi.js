9c6a9001823eb70612d57b997b9b2d6696666a5f
Pavel Lysenko <code@ohar.name>
2019-08-05 22:22:42 +0300

    Name generation: remove complicated schemes

--- src/components/DndNameGenerator/races/tabaxi/utils/generateFullNameTabaxi.js
+++ src/components/DndNameGenerator/races/tabaxi/utils/generateFullNameTabaxi.js
@@ -2,23 +2,15 @@ import _ from 'lodash'
 
 import schemaList, {
   ADJECTIVE_NOUN,
-  NOUN_OF_ADJECTIVE_NOUN,
   NOUN_OF_NOUN,
-  NOUN_OF_NUMBER_ADJECTIVE_NOUN,
   NOUN_OF_NUMBER_NOUN,
   NUMBER_ADJECTIVE_NOUN,
-  NUMBER_NOUN_OF_ADJECTIVE_NOUN,
-  NUMBER_NOUN_OF_NOUN,
 } from '@/constants/nameGeneration/nameSchemaList'
 
 import generateNameBySchemeAdjectiveNoun from '@/utils/nameGeneration/generateBySchemes/generateNameBySchemeAdjectiveNoun'
 import generateNameBySchemeNounOfNoun from '@/utils/nameGeneration/generateBySchemes/generateNameBySchemeNounOfNoun'
-import generateNameBySchemeNounOfAdjectiveNoun from '@/utils/nameGeneration/generateBySchemes/generateNameBySchemeNounOfAdjectiveNoun'
-import generateNameBySchemeNounOfNumberAdjectiveNoun from '@/utils/nameGeneration/generateBySchemes/generateNameBySchemeNounOfNumberAdjectiveNoun'
 import generateNameBySchemeNounOfNumberNoun from '@/utils/nameGeneration/generateBySchemes/generateNameBySchemeNounOfNumberNoun'
 import generateNameBySchemeNumberAdjectiveNoun from '@/utils/nameGeneration/generateBySchemes/generateNameBySchemeNumberAdjectiveNoun'
-import generateNameBySchemeNumberNounOfAdjectiveNoun from '@/utils/nameGeneration/generateBySchemes/generateNameBySchemeNumberNounOfAdjectiveNoun'
-import generateNameBySchemeNumberNounOfNoun from '@/utils/nameGeneration/generateBySchemes/generateNameBySchemeNumberNounOfNoun'
 
 // TODO
 // Кланы табакси
@@ -36,16 +28,8 @@ const generateFullNameTabaxi = ({gender, schema}) => {
       result = generateNameBySchemeAdjectiveNoun({gender})
       break
 
-    case NOUN_OF_ADJECTIVE_NOUN:
-      result = generateNameBySchemeNounOfAdjectiveNoun({gender})
-      break
-
     case NOUN_OF_NOUN:
       result = generateNameBySchemeNounOfNoun({gender})
-      break
-
-    case NOUN_OF_NUMBER_ADJECTIVE_NOUN:
-      result = generateNameBySchemeNounOfNumberAdjectiveNoun({gender})
       break
 
     case NOUN_OF_NUMBER_NOUN:
@@ -54,14 +38,6 @@ const generateFullNameTabaxi = ({gender, schema}) => {
 
     case NUMBER_ADJECTIVE_NOUN:
       result = generateNameBySchemeNumberAdjectiveNoun({gender})
-      break
-
-    case NUMBER_NOUN_OF_ADJECTIVE_NOUN:
-      result = generateNameBySchemeNumberNounOfAdjectiveNoun({gender})
-      break
-
-    case NUMBER_NOUN_OF_NOUN:
-      result = generateNameBySchemeNumberNounOfNoun({gender})
       break
 
     default:
--- src/constants/nameGeneration/nameSchemaList.js
+++ src/constants/nameGeneration/nameSchemaList.js
@@ -1,11 +1,7 @@
 export const ADJECTIVE_NOUN = 'adjective_noun'
-export const NOUN_OF_ADJECTIVE_NOUN = 'noun_of_adjective_noun'
 export const NOUN_OF_NOUN = 'noun_of_noun'
-export const NOUN_OF_NUMBER_ADJECTIVE_NOUN = 'noun_of_number_adjective_noun'
 export const NOUN_OF_NUMBER_NOUN = 'noun_of_number_noun'
 export const NUMBER_ADJECTIVE_NOUN = 'number_adjective_noun'
-export const NUMBER_NOUN_OF_ADJECTIVE_NOUN = 'number_noun_of_adjective_noun'
-export const NUMBER_NOUN_OF_NOUN = 'number_noun_of_noun'
 
 export default [
   {
@@ -27,21 +23,5 @@ export default [
   {
     text: 'Что-то скольки-то чего-то',
     value: NOUN_OF_NUMBER_NOUN,
-  },
-  {
-    text: 'Сколько-то чего-то чего-то',
-    value: NUMBER_NOUN_OF_NOUN,
-  },
-  {
-    text: 'Что-то какого-то чего-то',
-    value: NOUN_OF_ADJECTIVE_NOUN,
-  },
-  {
-    text: 'Что-то скольки-то какого-то чего-то',
-    value: NOUN_OF_NUMBER_ADJECTIVE_NOUN,
-  },
-  {
-    text: 'Сколько-то чего-то какого-то чего-то',
-    value: NUMBER_NOUN_OF_ADJECTIVE_NOUN,
   },
 ]

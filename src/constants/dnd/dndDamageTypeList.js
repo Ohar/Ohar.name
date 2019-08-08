d9c79f913bc670aa7111021c0096bf5fbc3fee3b
Pavel Lysenko <code@ohar.name>
2019-08-08 20:11:49 +0300

    dndDamageTypeList: aff fullnames

--- src/constants/dnd/dndDamageTypeList.js
+++ src/constants/dnd/dndDamageTypeList.js
@@ -18,54 +18,67 @@ const dndDamageTypeList = [
   {
     id: DAMAGE_BLUDGEONING,
     name: 'дробящий',
+    fullName: 'дробящий урон',
   },
   {
     id: DAMAGE_PIERCING,
     name: 'колющий',
+    fullName: 'колющий урон',
   },
   {
     id: DAMAGE_ACID,
     name: 'кислотой',
+    fullName: 'урон кислотой',
   },
   {
     id: DAMAGE_SLASHING,
     name: 'рубящий',
+    fullName: 'рубящий урон',
   },
   {
     id: DAMAGE_FIRE,
     name: 'огнём',
+    fullName: 'урон огнём',
   },
   {
     id: DAMAGE_COLD,
     name: 'холодом',
+    fullName: 'урон холодом',
   },
   {
     id: DAMAGE_FORCE,
     name: 'силовым полем',
+    fullName: 'урон силовым полем',
   },
   {
     id: DAMAGE_NECROTIC,
     name: 'некротический',
+    fullName: 'некротический урон',
   },
   {
     id: DAMAGE_LIGHTNING,
     name: 'молнией',
+    fullName: 'урон молнией',
   },
   {
     id: DAMAGE_POISON,
     name: 'ядом',
+    fullName: 'урон ядом',
   },
   {
     id: DAMAGE_PSYCHIC,
     name: 'психикой',
+    fullName: 'психический урон',
   },
   {
     id: DAMAGE_RADIANT,
     name: 'излучением',
+    fullName: 'урон излучением',
   },
   {
     id: DAMAGE_SONIC,
     name: 'звуком',
+    fullName: 'урон звуком',
   },
 ]
 

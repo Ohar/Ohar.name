4cd01c2d341f1d6650c9c3800afa257b77ce444d
Pavel Lysenko <code@ohar.name>
2019-08-10 00:28:48 +0300

    DndParam: separate calcParamBonus

--- src/components/DndCreature/components/DndParam/DndParamComponent.jsx
+++ src/components/DndCreature/components/DndParam/DndParamComponent.jsx
@@ -3,26 +3,22 @@ import PropTypes from "prop-types"
 
 import {dndParamCollection} from "@/constants/dnd/dndParamList"
 
-import calcParamBonus from "./utils/calcParamBonus"
+import calcParamBonus from "@/utils/calcParamBonus"
 
 import "./DndParamStyles.css"
 
 const DndParamComponent = ({ id, value }) => {
   const bonus = calcParamBonus(value)
-  const bonusAbs = Math.abs(bonus)
-  const bonusText = bonus < 0
-    ? `−${bonusAbs}`
-    : `+${bonusAbs}`
 
   return (
     <span className='DndParam'>
-    <b className='DndParam_header'>
-      {dndParamCollection[id].shortName}
-    </b>
-    <span className='DndParam_value'>
-      {value} ({bonusText})
+      <b className='DndParam_header'>
+        {dndParamCollection[id].shortName}
+      </b>
+      <span className='DndParam_value'>
+        {value} ({bonus})
+      </span>
     </span>
-  </span>
   )
 }
 
--- src/components/DndCreature/components/DndParam/utils/calcParamBonus.js
+++
@@ -1,6 +0,0 @@
-const NORMAL_PARAM = 10
-const KOEF_STEP = 2
-
-const calcParamBonus = val => Math.floor((val - NORMAL_PARAM) / KOEF_STEP)
-
-export default calcParamBonus

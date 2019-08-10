ef28afe7d92db3d801af7fbf7d8740389befba3d
Pavel Lysenko <code@ohar.name>
2019-08-10 16:40:50 +0300

    deploy config: move into deploy folder

--- shell_scripts/deploy.sh
+++ shell_scripts/deploy.sh
@@ -5,9 +5,9 @@
 declare -r PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$PARENT_PATH"
 
-declare -r FOLDER=`node -p 'require("./../config.json")["path"]'`;
-declare -r SERVER=`node -p 'require("./../config.json")["server"]'`;
-declare -r USERNAME=`node -p 'require("./../config.json")["username"]'`;
+declare -r FOLDER=`node -p 'require("./config.json")["path"]'`;
+declare -r SERVER=`node -p 'require("./config.json")["server"]'`;
+declare -r USERNAME=`node -p 'require("./config.json")["username"]'`;
 
 declare -r URL=$USERNAME@$SERVER
 

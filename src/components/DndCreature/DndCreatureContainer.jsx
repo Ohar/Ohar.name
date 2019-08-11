cec2fddfe7852a3fa77713480a31d8547ceccf7b
Pavel Lysenko <code@ohar.name>
2019-08-11 23:23:24 +0300

    DndCreatureContainer: remove exceed props

--- src/components/DndCreature/DndCreatureContainer.jsx
+++ src/components/DndCreature/DndCreatureContainer.jsx
@@ -11,34 +11,15 @@ import DndCreatureComponent from "./DndCreatureComponent"
 const DndCreatureContainer = (
   {
     creature: {
-      actionList,
       aligmentId,
-      armor,
-      cr,
       creatureTypeIdList,
-      description,
-      featureList,
-      hp,
-      isFemale,
-      languageList,
-      legendaryActionList,
-      legendaryPoints,
-      name,
-      nameEn,
-      params,
-      reactionList,
-      saveThrowCollection,
-      senseList,
       sizeType,
-      skillCollection,
-      source,
-      speed,
-      spellCast,
+      ...rest,
     },
   },
 ) => {
-  const size = dndSizeCollection[sizeType].name
-  const aligment = dndAligmentCollection[aligmentId].name.nominative
+  const {name: size} = dndSizeCollection[sizeType]
+  const {name: {nominative: aligment}} = dndAligmentCollection[aligmentId]
   const creatureType = [
     getCreatureTypeName(creatureTypeIdList[0]),
     creatureTypeIdList.length > 1
@@ -55,32 +36,10 @@ const DndCreatureContainer = (
 
   return (
     <DndCreatureComponent
-      actionList={actionList}
       aligment={aligment}
-      aligmentId={aligmentId}
-      armor={armor}
-      cr={cr}
       creatureType={creatureType}
-      creatureTypeIdList={creatureTypeIdList}
-      description={description}
-      featureList={featureList}
-      hp={hp}
-      isFemale={isFemale}
-      languageList={languageList}
-      legendaryActionList={legendaryActionList}
-      legendaryPoints={legendaryPoints}
-      name={name}
-      nameEn={nameEn}
-      params={params}
-      reactionList={reactionList}
-      saveThrowCollection={saveThrowCollection}
-      senseList={senseList}
       size={size}
-      sizeType={sizeType}
-      skillCollection={skillCollection}
-      source={source}
-      speed={speed}
-      spellCast={spellCast}
+      {...rest}
     />
   )
 }

3f301ad5862379014e78e8a2e91070c7d1c6749a
Pavel Lysenko <code@ohar.name>
2019-08-11 15:03:20 +0300

    dndParamList: format shortNames

--- src/constants/dnd/dndParamList.js
+++ src/constants/dnd/dndParamList.js
@@ -11,32 +11,32 @@ const dndParamList = [
   {
     id: PARAM_STR,
     name: 'Сила',
-    shortName: 'СИЛ',
+    shortName: 'Сил',
   },
   {
     id: PARAM_DEX,
     name: 'Ловкость',
-    shortName: 'ЛОВ',
+    shortName: 'Лов',
   },
   {
     id: PARAM_CON,
     name: 'Телосложение',
-    shortName: 'ТЕЛ',
+    shortName: 'Тел',
   },
   {
     id: PARAM_INT,
     name: 'Интеллект',
-    shortName: 'ИНТ',
+    shortName: 'Инт',
   },
   {
     id: PARAM_WIT,
     name: 'Мудрость',
-    shortName: 'МУД',
+    shortName: 'Мдр',
   },
   {
     id: PARAM_CHA,
     name: 'Харизма',
-    shortName: 'ХАР',
+    shortName: 'Хар',
   },
 ]
 

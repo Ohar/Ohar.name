247f9fecf7ad72bf4a390c5ca64cc3cacb22505c
Pavel Lysenko <code@ohar.name>
2019-08-15 01:44:06 +0300

    formatRangeText: handle spell attak

--- src/components/DndCreature/components/DndActionList/utils/formatRangeText.js
+++ src/components/DndCreature/components/DndActionList/utils/formatRangeText.js
@@ -3,10 +3,12 @@ import {
   ACTION_MELEE_SPELL_ATTACK,
   ACTION_MELEE_OR_RANGE_WEAPON_ATTACK,
   ACTION_RANGE_WEAPON_ATTACK,
+  ACTION_RANGE_SPELL_ATTACK,
 } from '@/constants/dnd/dndActionTypeList'
 
 const formatMelee = range => `досягаемость ${range} фт.`
 const formatRange = ({normal, max}) => `дистанция ${normal} фт./${max} фт.`
+const formatRangeSpell = range => `дистанция ${range} фт.`
 
 export default ({type, range}) => {
   switch (type) {
@@ -23,6 +25,9 @@ export default ({type, range}) => {
 
     case ACTION_RANGE_WEAPON_ATTACK:
       return formatRange(range)
+
+    case ACTION_RANGE_SPELL_ATTACK:
+      return formatRangeSpell(range)
 
     default:
       return ''

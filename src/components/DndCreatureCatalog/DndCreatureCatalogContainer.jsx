86c30c35709b4b00ae199d1718c7ccb92b0c71bd
Pavel Lysenko <code@ohar.name>
2019-08-15 18:52:44 +0300

    DndCreatureCatalogComponent: fix code style, remove exceed props from render()

--- src/components/DndCreatureCatalog/DndCreatureCatalogContainer.jsx
+++ src/components/DndCreatureCatalog/DndCreatureCatalogContainer.jsx
@@ -1,54 +1,56 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
 
-import config from 'root/config'
+import config from 'root/config';
 
-import SEARCH_PROP_NAME from '@/constants/SEARCH_PROP_NAME'
-import dndCreatureList from '@/constants/dnd/dndCreatureList'
+import SEARCH_PROP_NAME from '@/constants/SEARCH_PROP_NAME';
+import dndCreatureList from '@/constants/dnd/dndCreatureList';
 
-import prepareForSearch from "@/utils/prepareForSearch"
+import prepareForSearch from '@/utils/prepareForSearch';
 
-import DndCreatureCatalogComponent from './DndCreatureCatalogComponent'
+import DndCreatureCatalogComponent from './DndCreatureCatalogComponent';
 
-const MAX_LEN = 20
+const MAX_LEN = 20;
 
 class DndCreatureCatalogContainer extends Component {
-    state = {
-        goodsFilteredList: [],
-        isTooMuch: false,
+  state = {
+    filteredCreatureList: [],
+    isTooMuch: false
+  };
+
+  // TODO: remove after ready
+  componentDidMount() {
+    if (config) {
+      const { creatureCatalogDefaultSearchString = '' } = config;
+      this.onSearch({ target: { value: creatureCatalogDefaultSearchString } });
     }
+  }
 
-    // TODO: remove after ready
-    componentDidMount() {
-        if (config) {
-            const {creatureCatalogDefaultSearchString = ''} = config
-            this.onSearch({target: {value: creatureCatalogDefaultSearchString}})
-        }
-    }
-
-    onSearch = ({target: {value}}) => {
-        const filteredCreatureList = value
-            ? dndCreatureList
-                .filter(
-                    item => item[SEARCH_PROP_NAME].includes(prepareForSearch(value))
-                )
-            : []
-
-        const isTooMuch = filteredCreatureList.length > MAX_LEN
-
-        this.setState({
-            filteredCreatureList: filteredCreatureList.slice(0, MAX_LEN),
-            isTooMuch,
-        })
-    }
-
-    render() {
-        return (
-            <DndCreatureCatalogComponent
-                onSearch={this.onSearch}
-                {...this.state}
-            />
+  onSearch = ({ target: { value } }) => {
+    const filteredCreatureList = value
+      ? dndCreatureList
+        .filter(
+          item => item[SEARCH_PROP_NAME].includes(prepareForSearch(value))
         )
-    }
+      : [];
+
+    const isTooMuch = filteredCreatureList.length > MAX_LEN;
+
+    this.setState({
+      filteredCreatureList: filteredCreatureList.slice(0, MAX_LEN),
+      isTooMuch
+    });
+  };
+
+  render() {
+    const { filteredCreatureList } = this.state;
+
+    return (
+      <DndCreatureCatalogComponent
+        onSearch={this.onSearch}
+        filteredCreatureList={filteredCreatureList}
+      />
+    );
+  }
 }
 
-export default DndCreatureCatalogContainer
+export default DndCreatureCatalogContainer;

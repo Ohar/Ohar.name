ba6c272ea3754bdc3726dd64dc4031fc3b0621d3
Pavel Lysenko <code@ohar.name>
2019-08-22 03:03:15 +0300

    DndCreatureHeaderComponent: fix build

--- src/components/DndCreature/components/DndCreatureHeader/DndCreatureHeaderComponent.jsx
+++ src/components/DndCreature/components/DndCreatureHeader/DndCreatureHeaderComponent.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 import { Link } from 'gatsby'
 
 import generateCreaturePageUrlById from '@/utils/generateCreaturePageUrlById'
@@ -7,8 +8,11 @@ import './DndCreatureHeaderStyles.less'
 
 const DndCreatureHeaderComponent = ({ id, title, header }) => {
   const url = generateCreaturePageUrlById(id)
+  const pathname = typeof window === 'undefined'
+    ? ''
+    : window.location.pathname
 
-  return url === window.location.pathname
+  return url === pathname
     ? (
       <Link
         to={url}
@@ -26,6 +30,17 @@ const DndCreatureHeaderComponent = ({ id, title, header }) => {
         {header}
       </span>
     )
+}
+
+DndCreatureHeaderComponent.propTypes = {
+  header: PropTypes.string,
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string,
+}
+
+DndCreatureHeaderComponent.defaultProps = {
+  header: '',
+  title: '',
 }
 
 export default DndCreatureHeaderComponent

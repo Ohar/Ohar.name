74a853474140607950d537f8b97136a8c690d2ac
Pavel Lysenko <code@ohar.name>
2019-08-28 12:51:03 +0300

    calculateColumnCount: count description text length for columns calculation

--- src/components/DndCreature/utils/calculateColumnCount.js
+++ src/components/DndCreature/utils/calculateColumnCount.js
@@ -1,18 +1,32 @@
-const COEF_ITEM_PER_COLUMN = 4
+const COEF_ITEM_PER_COLUMN = 5
 const MAX_COLUMN = 4
+const DESCRIPTION_COEF = 1/400
+const NO_DESCRIPTION_WEIGNT = 0
+const ITEM_WEIGNT = 1
 
 export default (
   {
+    actionList = [],
+    featureList = [],
+    legendaryActionList = [],
+    legendaryPoints = 0,
+  }
+) => {
+  const total = [
     actionList,
     featureList,
-    legendaryPoints,
     legendaryActionList,
-  }) => {
-  const featureCount = featureList && featureList.length || 0
-  const actionCount = actionList && actionList.length || 0
-  const legendaryActionCount = legendaryActionList && legendaryActionList.length || 0
-
-  const total = featureCount + actionCount + legendaryActionCount
+  ].reduce(
+    (listSum, listItem) => listSum + listItem.reduce(
+      (sum, item) => ITEM_WEIGNT + sum + (
+        item.description
+          ? item.description.length * DESCRIPTION_COEF
+          : NO_DESCRIPTION_WEIGNT
+      ),
+      0
+    ),
+    0
+  )
 
   for (let i = MAX_COLUMN; i > 1; i--) {
     if (total > (i - 1) * COEF_ITEM_PER_COLUMN) {

c1254ab980403f4fe4d8a315a36ad9ee021635cb
Pavel Lysenko <code@ohar.name>
2019-08-28 03:31:17 +0300

    DndLegendaryActionListContainer: handle only single legendary action

--- src/components/DndCreature/components/DndLegendaryActionList/DndLegendaryActionListContainer.jsx
+++ src/components/DndCreature/components/DndLegendaryActionList/DndLegendaryActionListContainer.jsx
@@ -14,25 +14,35 @@ const DndLegendaryActionListContainer = (
   {
     name,
     points,
+    list,
     ...rest,
   }
 ) => {
-  const entryText = `${name} может совершить ${points} ${getLegActionWord(points)} выбирая из представленных ниже вариантов. За один раз можно использовать только одно легендарное действие, и только в конце хода другого существа. ${name} восстанавливает использованные легендарные действия в начале своего хода.`
+  const variantText = list.length === 1
+    ? `используя описанный ниже ${list[0].name}`
+    : 'выбирая из представленных ниже вариантов'
+  const entryText = `${name} может совершить ${points} ${getLegActionWord(points)}, ${variantText}. За один раз можно использовать только одно легендарное действие, и только в конце хода другого существа. ${name} восстанавливает использованные легендарные действия в начале своего хода.`
 
   return points
     ? (
       <DndLegendaryActionListComponent
         entry={entryText}
         points={points}
+        list={list}
         {...rest}
       />
     )
     : null
 }
 
+DndLegendaryActionListContainer.defaultProps = {
+  list: [],
+}
+
 DndLegendaryActionListContainer.propTypes = {
   name: PropTypes.string.isRequired,
   points: PropTypes.number.isRequired,
+  list: PropTypes.array,
 }
 
 export default DndLegendaryActionListContainer

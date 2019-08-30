533fa18964bb507d57087f5b2fe099999a284ba9
Pavel Lysenko <code@ohar.name>
2019-08-30 19:02:17 +0300

    DndCreatureDescription: hide when no text

--- src/components/DndCreature/components/DndCreatureDescription/DndCreatureDescriptionContainer.jsx
+++ src/components/DndCreature/components/DndCreatureDescription/DndCreatureDescriptionContainer.jsx
@@ -6,29 +6,33 @@ const COLUMN_COEF = 1500
 const MAX_COLUMN = 3
 const MAX_LEN_TO_HANDLE = MAX_COLUMN * COLUMN_COEF
 
-const DndCreatureDescriptionContainer = ({name, nameAlt, nameEn, nameEnAlt, description = '', ...rest}) => {
-  const nameEnAltText = nameEnAlt
-    ? ` (${nameEnAlt})`
-    : ''
-  const nameAltText = nameAlt
-    ? ` (${nameAlt})`
-    : ''
-  const nameText = `${name}${nameAltText}`
-  const nameEnText = `${nameEn}${nameEnAltText}`
+const DndCreatureDescriptionContainer = ({name, nameAlt, nameEn, nameEnAlt, description, ...rest}) => {
+  if (description) {
+    const nameEnAltText = nameEnAlt
+      ? ` (${nameEnAlt})`
+      : ''
+    const nameAltText = nameAlt
+      ? ` (${nameAlt})`
+      : ''
+    const nameText = `${name}${nameAltText}`
+    const nameEnText = `${nameEn}${nameEnAltText}`
 
-  const columnCount = description.length > MAX_LEN_TO_HANDLE
-    ? MAX_COLUMN
-    : Math.ceil(description.length / COLUMN_COEF)
+    const columnCount = description.length > MAX_LEN_TO_HANDLE
+      ? MAX_COLUMN
+      : Math.ceil(description.length / COLUMN_COEF)
 
-  return (
-    <DndCreatureDescriptionComponent
-      nameText={nameText}
-      nameEnText={nameEnText}
-      description={description}
-      columnCount={columnCount}
-      {...rest}
-    />
-  )
+    return (
+      <DndCreatureDescriptionComponent
+        nameText={nameText}
+        nameEnText={nameEnText}
+        description={description}
+        columnCount={columnCount}
+        {...rest}
+      />
+    )
+  }
+
+  return null
 }
 
 export default DndCreatureDescriptionContainer

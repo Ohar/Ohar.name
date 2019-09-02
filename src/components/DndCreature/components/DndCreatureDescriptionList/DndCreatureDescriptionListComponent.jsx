b151b2d108cd441635c6e2d966aa6c2015c59e87
Pavel Lysenko <code@ohar.name>
2019-09-02 18:42:55 +0300

    DndCreatureDescription: handle arrays and objects

---
+++ src/components/DndCreature/components/DndCreatureDescriptionList/DndCreatureDescriptionListComponent.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+import DndCreatureDescription from './../DndCreatureDescription'
+
+const DndCreatureDescriptionListComponent = ({description}) => (
+  <>
+    {
+      description.map(
+        item => (
+          <DndCreatureDescription {...item}/>
+        )
+      )
+    }
+  </>
+)
+
+DndCreatureDescriptionListComponent.propTypes = {
+  description: PropTypes.array,
+}
+
+DndCreatureDescriptionListComponent.defaultProps = {
+  description: [],
+}
+
+export default DndCreatureDescriptionListComponent

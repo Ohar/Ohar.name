6a361c067265bc8c6f49b9148137a17cc39354c4
Pavel Lysenko <code@ohar.name>
2019-09-09 04:33:23 +0300

    extendCreature: fix code style

--- src/utils/extendCreature.js
+++ src/utils/extendCreature.js
@@ -3,65 +3,60 @@ const filterData = require('./filterData')
 const extendCreature = ({creature, parent}) => {
   const RESULT_DEFAULT = {}
 
-  const filterPropNameList = creature.filterPropCollection
-    ? Object.keys(creature.filterPropCollection)
-    : []
+  const filteredParent = Object
+    .keys(creature.filterPropCollection || {})
+    .reduce(
+      (parentObj, propName) => ({
+        ...parentObj,
+        [propName]: filterData(
+          {
+            filters: creature.filterPropCollection[propName],
+            data: parent[propName],
+          }
+        ),
+      }),
+      {...parent}
+    )
 
-  const filteredParent = filterPropNameList.reduce(
-    (parentObj, propName) => ({
-      ...parentObj,
-      [propName]: filterData(
-        {
-          filters: creature.filterPropCollection[propName],
-          data: parent[propName],
-        }
-      ),
-    }),
-    {...parent}
-  )
+  const editedParent = Object
+    .keys(creature.editPropCollection || {})
+    .reduce(
+      (resultObj, propName) => ({
+        ...resultObj,
+        [propName]: creature.editPropCollection[propName](filteredParent),
+      }),
+      {}
+    )
 
-  const editPropNameList = creature.editPropCollection
-    ? Object.keys(creature.editPropCollection)
-    : []
+  const extended = Object
+    .keys(creature.extendPropCollection || {})
+    .reduce(
+      (result, listName) => {
+        const {extendPropCollection} = creature
 
-  const editedParent = editPropNameList.reduce(
-    (resultObj, propName) => ({
-      ...resultObj,
-      [propName]: creature.editPropCollection[propName](filteredParent),
-    }),
-    {}
-  )
-
-  const extendPropNameList = creature.extendPropCollection
-    ? Object.keys(creature.extendPropCollection)
-    : []
-
-  const extended = extendPropNameList.reduce(
-    (result, listName) => {
-      const {extendPropCollection} = creature
-
-      return extendPropCollection[listName]
-        ? {
-          ...result,
-          [listName]: Array.isArray(extendPropCollection[listName])
-            ? [
-              ...(filteredParent[listName] || []),
-              ...extendPropCollection[listName],
-            ]
-            : {
-              ...(filteredParent[listName] || {}),
-              ...extendPropCollection[listName],
-            },
-        }
-        : result
-    },
-    RESULT_DEFAULT
-  )
+        return extendPropCollection[listName]
+          ? {
+            ...result,
+            [listName]: Array.isArray(extendPropCollection[listName])
+              ? [
+                ...(filteredParent[listName] || []),
+                ...extendPropCollection[listName],
+              ]
+              : {
+                ...(filteredParent[listName] || {}),
+                ...extendPropCollection[listName],
+              },
+          }
+          : result
+      },
+      RESULT_DEFAULT
+    )
 
   const resultObj = (
     extended === RESULT_DEFAULT
-    && !filterPropNameList.length
-    && !editPropNameList.length
+    && !creature.extendPropCollection
+    && !creature.filterPropCollection
+    && !creature.editPropCollection
   )
     ? creature
     : {
@@ -71,19 +66,17 @@ const extendCreature = ({creature, parent}) => {
       ...editedParent,
     }
 
-  const replaceEmptyPropNameList = creature.replaceEmptyPropCollection
-    ? Object.keys(creature.replaceEmptyPropCollection)
-    : []
+  const replaceEmptyParent = Object
+    .keys(creature.replaceEmptyPropCollection || {})
+    .reduce(
+      (parentObj, propName) => ({
+        ...parentObj,
+        [propName]: parentObj[propName] || creature.replaceEmptyPropCollection[propName](parentObj),
+      }),
+      {...resultObj}
+    )
 
-  const replaceEmptyParent = replaceEmptyPropNameList.reduce(
-    (parentObj, propName) => ({
-      ...parentObj,
-      [propName]: parentObj[propName] || creature.replaceEmptyPropCollection[propName](parentObj),
-    }),
-    {...resultObj}
-  )
-
-  return replaceEmptyPropNameList.length
+  return creature.replaceEmptyPropCollection
     ? replaceEmptyParent
     : resultObj
 }

81aad59339cd2e3da42f29d9211285e169181e5b
Pavel Lysenko <code@ohar.name>
2019-09-09 19:03:25 +0300

    dndCreatureTemplateList: add “Shadow dragon” template

--- src/components/DndCreature/components/DndCreatureStats/components/DndCreatureExtender/constants/dndCreatureTemplateList.js
+++ src/components/DndCreature/components/DndCreatureStats/components/DndCreatureExtender/constants/dndCreatureTemplateList.js
@@ -1,4 +1,8 @@
-import { faDizzy, faSkullCrossbones } from '@fortawesome/free-solid-svg-icons'
+import arrify from "arrify"
+import { faDizzy, faSkullCrossbones, faMehBlank } from '@fortawesome/free-solid-svg-icons'
+
+import generateTextLinks from '@/utils/generateTextLinks'
+
 import {
   SIZE_MEDIUM,
   SIZE_LARGE,
@@ -65,6 +69,7 @@ import {
   ACTION_RANGE_SPELL_ATTACK
 } from '@/constants/dnd/dndActionTypeList'
 import {
+  dndDamageTypeCollection,
   DAMAGE_ACID,
   DAMAGE_BLUDGEONING,
   DAMAGE_COLD,
@@ -88,9 +93,11 @@ import {
 } from '@/constants/dnd/dndSenseList'
 import {
   PARAM_STR,
+  PARAM_DEX,
+  PARAM_CON,
   PARAM_INT,
   PARAM_WIT,
-  PARAM_CHA
+  PARAM_CHA,
 } from '@/constants/dnd/dndParamList'
 import {
   CONDITION_BLINDED,
@@ -122,7 +129,42 @@ import {
   CREATURE_ANCIENT_RED_DRAGON,
   CREATURE_ANCIENT_SILVER_DRAGON,
   CREATURE_ANCIENT_WHITE_DRAGON,
+  CREATURE_BLACK_DRAGON_WYRMLING,
+  CREATURE_BLUE_DRAGON_WYRMLING,
+  CREATURE_BRASS_DRAGON_WYRMLING,
+  CREATURE_BRONZE_DRAGON_WYRMLING,
+  CREATURE_COPPER_DRAGON_WYRMLING,
+  CREATURE_GOLD_DRAGON_WYRMLING,
+  CREATURE_GREEN_DRAGON_WYRMLING,
+  CREATURE_RED_DRAGON_WYRMLING,
+  CREATURE_SILVER_DRAGON_WYRMLING,
+  CREATURE_WHITE_DRAGON_WYRMLING,
+  CREATURE_YOUNG_BLACK_DRAGON,
+  CREATURE_YOUNG_BLUE_DRAGON,
+  CREATURE_YOUNG_BRASS_DRAGON,
+  CREATURE_YOUNG_BRONZE_DRAGON,
+  CREATURE_YOUNG_COPPER_DRAGON,
+  CREATURE_YOUNG_GOLD_DRAGON,
+  CREATURE_YOUNG_GREEN_DRAGON,
+  CREATURE_YOUNG_RED_DRAGON,
+  CREATURE_YOUNG_SILVER_DRAGON,
+  CREATURE_YOUNG_WHITE_DRAGON,
 } from '@/constants/dnd/dndCreatureIdList'
+import {
+  SKILL_ARCANA,
+  SKILL_ATHLETICS,
+  SKILL_DECEPTION,
+  SKILL_HISTORY,
+  SKILL_INSIGHT,
+  SKILL_INTIMIDATION,
+  SKILL_INVESTIGATION,
+  SKILL_MEDICINE,
+  SKILL_PERCEPTION,
+  SKILL_PERSUASION,
+  SKILL_RELIGION,
+  SKILL_STEALTH,
+  SKILL_SURVIVAL,
+} from '@/constants/dnd/dndSkillList'
 
 import {dndCrCollection} from "@/constants/dnd/dndCrList"
 
@@ -222,7 +264,8 @@ export default [
         CREATURE_TYPE_XVART,
         CREATURE_TYPE_YOAN_TI,
         CREATURE_TYPE_YUGOLOT
-      ]
+      ],
+      templateName: ['Теневой дракон'],
     },
 
     editPropCollection: {
@@ -378,6 +421,7 @@ export default [
       creatureTypeIdList: [
         CREATURE_TYPE_DRAGON,
       ],
+      templateName: ['Теневой дракон'],
     },
 
     extendPropCollection: {
@@ -398,6 +442,176 @@ export default [
         {
           name: 'Сопротивление магии',
           description: `Драколич совершает с преимуществом спасброски от заклинаний и прочих магических эффектов.`,
+        },
+      ],
+    },
+  },
+  {
+    templateName: 'Теневой дракон',
+    templateIcon: faMehBlank,
+
+    source: 'MM:100',
+    description: [
+      {
+        header: 'Шаблон теневого дракона',
+        text: `Только истинный дракон может превратиться в теневого дракона, и только в том случае, если рождается в Царстве Теней или проживает там несколько лет. Драколич не может стать теневым драконом, так как он уже потерял свою драконью природу и стал нежитью.\n
+Когда дракон становится теневым драконом, он сохраняет свою статистику, за исключением пунктов, описанных ниже. Теневой дракон может сохранить или потерять некоторые или все свои действия логова, или унаследовать новые, в зависимости от решения Мастера.\n
+**Сопротивление к урону.** Теневой дракон получает сопротивление к урону некротической энергией.\n
+**Владение навыком: Скрытность.** Бонус мастерства дракона удваивается для его проверок Ловкости (Скрытность).\n
+**Живая тень.** Находясь в области тусклого свете или тьмы, дракон обладает сопротивлением ко всем видам урона кроме урона излучением, психической энергией и силовым полем.\n
+**Скрытность в тени.** Находясь в области тусклого света или тьмы, дракон может совершать действие Засада бонусным действием.\n
+**Чувствительность к солнечному свету.** Находясь на солнечном свету, дракон совершает с помехой броски атаки, а также проверки Мудрости (Внимательность), полагающиеся на зрение.\n
+**Новое действие: Укус.** Если дракон причиняет укусом урон кислотой, огнём, холодом, электричеством или ядом, измените этот урон на урон некротической энергией.\n
+**Новое действие: Теневое дыхание.** Любое причиняющее урон оружие дыхания, имеющееся у дракона, теперь причиняет урон некротической энергией, вместо первоначального вида урона. Гуманоид, хиты которого уменьшились до 0 от этого урона, умирает, и из его трупа выходит [тень](CREATURE:shadow) — нежить, действующая сразу после дракона по инициативе. Эта тень находится под контролем дракона.`,
+      },
+      {
+        header: 'Теневой дракон',
+        text: `Теневые драконы это истинные драконы, которые либо родились в Царстве Теней, либо трансформировались за годы, проведённые в его мрачных пределах. Некоторые теневые драконы остаются в Царстве Теней, так как им нравятся его безрадостные пейзажи и запустение. Другие стремятся вернуться на Материальный План, желая распространить там тьму и зло Царства Теней.\n
+**Тёмные порталы.** Порталы в Царство Теней появляются в покинутых местах и глубочайшем мраке подземных каверн. Драконы, устраивающие логова в подобных местах, часто находят такие порталы и переносятся на новый план. Древние драконы, которые спят в своих берлогах на протяжении месяцев и лет, могут проснуться и понять, что пока они видели сны, без их ведома сформировался перенёсший их портал.\n
+**Перерождение в тень.** Превращение дракона из обычного в теневого происходит за нескольких лет, в течение которых чешуйки постепенно утрачивают свой блеск и выцветают до тёмных, угольных оттенков. Его кожистые крылья становятся полупрозрачными, его глаза превращаются в омуты, переливающиеся серым. Теневые драконы считают солнечный свет отвратительным; при ярком свете они слабее, чем во тьме, а в темноте они принимают облик, более похожий на их былую внешность.\n
+Магическая природа драконов привлекательна для Царства Теней, которое, как кажется, каким-то образом жаждет мощь и величие этих гигантских рептилий. Царство Теней также оказывает угнетающее влияние на своих обитателей, так что чем дольше существо остаётся на этом плане, тем больше оно перенимает общую тоску. Месяцы и годы проходят для дракона в Царстве Теней, он осознаёт свою трансформацию, но всё же ничего не делает, чтобы предотвратить её.\n
+**Назад на Материальный План.** Теневые драконы переполнены силой Царства Теней настолько, что даже возврат на Материальный План не может отменить их трансформацию. Некоторые теневые драконы пытаются заманить других существ из мира смертных обратно в Царство Теней, чтобы пообщаться с ними, по крайней мере, пока они не устанут от своих гостей и не съедят их. Другие рады оставить Царство Теней навсегда позади, понимая, что сокровища и власть легче найти на Материальном Плане.`,
+      },
+    ],
+
+    editPropCollection: {
+      name: ({name}) => name.includes('Вирмлинг')
+        ? name.replace('Вирмлинг', 'Теневой вирмлинг')
+        : name.replace('дракон', 'теневой дракон'),
+      skillCollection: ({skillCollection, cr, params}) => {
+        const {profBonus} = dndCrCollection[cr]
+        const dexBonus = calcParamBonus(params[PARAM_DEX])
+
+        return {
+          ...(skillCollection || {}),
+          [SKILL_STEALTH]: dexBonus + profBonus * 2,
+        }
+      },
+      actionList: ({actionList}) => actionList.map(
+        action => {
+          if (action.name === 'Укус') {
+            const hit = arrify(action.attack.hit)
+              .map(
+                hitItem => arrify(hitItem)
+                  .map(
+                    item => {
+                      if (
+                        [
+                          DAMAGE_ACID,
+                          DAMAGE_COLD,
+                          DAMAGE_ELECTRICITY,
+                          DAMAGE_FIRE,
+                          DAMAGE_POISON,
+                        ]
+                          .includes(item.type)
+                      ) {
+                        return {
+                          ...item,
+                          type: DAMAGE_NECROTIC,
+                        }
+                      }
+
+                      return item
+                    }
+                  )
+              )
+
+            return {
+              ...action,
+              attack: {
+                ...action.attack,
+                hit,
+              },
+            }
+          }
+
+          if (action.name.toLowerCase().includes('дыхание')) {
+            const changedDescription = action.description
+              .replace(
+                /(огонь)|(волну ледяного воздуха)|(молнию)|(кислоту)|(ядовитый газ)/g,
+                'некротическую энергию'
+              )
+              .replace(
+                /(огнём)|(холодом)|(электричеством)|(кислотой)|(ядом)/g,
+                'некротической энергией'
+              )
+            const description = generateTextLinks(`${changedDescription}. Гуманоид, хиты которого уменьшились до 0 от этого урона, умирает, и из его трупа выходит [тень](CREATURE:shadow) — нежить, действующая сразу после дракона по инициативе. Эта тень находится под контролем дракона.`)
+
+            return {
+              ...action,
+              name: 'Тёмное дыхание',
+              description,
+            }
+          }
+
+          return action
+        }
+      ),
+    },
+
+    extendLimitations: {
+      creatureIdList: [
+        CREATURE_ADULT_BLACK_DRAGON,
+        CREATURE_ADULT_BLUE_DRAGON,
+        CREATURE_ADULT_BRASS_DRAGON,
+        CREATURE_ADULT_BRONZE_DRAGON,
+        CREATURE_ADULT_COPPER_DRAGON,
+        CREATURE_ADULT_GOLD_DRAGON,
+        CREATURE_ADULT_GREEN_DRAGON,
+        CREATURE_ADULT_RED_DRAGON,
+        CREATURE_ADULT_SILVER_DRAGON,
+        CREATURE_ADULT_WHITE_DRAGON,
+        CREATURE_ANCIENT_BLACK_DRAGON,
+        CREATURE_ANCIENT_BLUE_DRAGON,
+        CREATURE_ANCIENT_BRASS_DRAGON,
+        CREATURE_ANCIENT_BRONZE_DRAGON,
+        CREATURE_ANCIENT_COPPER_DRAGON,
+        CREATURE_ANCIENT_GOLD_DRAGON,
+        CREATURE_ANCIENT_GREEN_DRAGON,
+        CREATURE_ANCIENT_RED_DRAGON,
+        CREATURE_ANCIENT_SILVER_DRAGON,
+        CREATURE_ANCIENT_WHITE_DRAGON,
+        CREATURE_BLACK_DRAGON_WYRMLING,
+        CREATURE_BLUE_DRAGON_WYRMLING,
+        CREATURE_BRASS_DRAGON_WYRMLING,
+        CREATURE_BRONZE_DRAGON_WYRMLING,
+        CREATURE_COPPER_DRAGON_WYRMLING,
+        CREATURE_GOLD_DRAGON_WYRMLING,
+        CREATURE_GREEN_DRAGON_WYRMLING,
+        CREATURE_RED_DRAGON_WYRMLING,
+        CREATURE_SILVER_DRAGON_WYRMLING,
+        CREATURE_WHITE_DRAGON_WYRMLING,
+        CREATURE_YOUNG_BLACK_DRAGON,
+        CREATURE_YOUNG_BLUE_DRAGON,
+        CREATURE_YOUNG_BRASS_DRAGON,
+        CREATURE_YOUNG_BRONZE_DRAGON,
+        CREATURE_YOUNG_COPPER_DRAGON,
+        CREATURE_YOUNG_GOLD_DRAGON,
+        CREATURE_YOUNG_GREEN_DRAGON,
+        CREATURE_YOUNG_RED_DRAGON,
+        CREATURE_YOUNG_SILVER_DRAGON,
+        CREATURE_YOUNG_WHITE_DRAGON,
+      ],
+      creatureTypeIdList: [CREATURE_TYPE_DRAGON],
+      templateName: ['Драколич'],
+    },
+
+    extendPropCollection: {
+      resistanceList: [
+        DAMAGE_NECROTIC,
+      ],
+      featureList: [
+        {
+          name: 'Чувствительность к солнечному свету',
+          description: `Находясь на солнечном свету, дракон совершает с помехой броски атаки, а также проверки Мудрости (Внимательность), полагающиеся на зрение.`,
+        },
+        {
+          name: 'Живая тень',
+          description: `Находясь в области тусклого свете или тьмы, дракон обладает сопротивлением ко всем видам урона кроме урона излучением, психической энергией и силовым полем.`,
+        },
+        {
+          name: 'Скрытность в тени',
+          description: `Находясь в области тусклого света или тьмы, дракон может совершать действие Засада бонусным действием.`,
         },
       ],
     },

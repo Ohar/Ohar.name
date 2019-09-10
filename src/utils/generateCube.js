a45f9e83eed734e6a3a144fb2394433c80ced085
Pavel Lysenko <code@ohar.name>
2019-09-10 16:47:17 +0300

    generateCube: allow to use max

--- src/utils/generateCube.js
+++ src/utils/generateCube.js
@@ -3,13 +3,17 @@ export default cube => {
     const {
       cubeType = 0,
       cubeCount = 0,
-      cubeBonus = 0
+      cubeBonus = 0,
+      useMax = false,
     } = cube
 
-    const average = Math.max(
-      cubeCount + cubeBonus + Math.floor((cubeType * cubeCount - cubeCount) / 2),
-      0
-    )
+    const average = useMax
+      ? cubeType * cubeCount + cubeBonus
+      : Math.max(
+        cubeCount + cubeBonus + Math.floor((cubeType * cubeCount - cubeCount) / 2),
+        0
+      )
+
     const count = cubeCount
       ? `${cubeCount}к${cubeType}`
       : ''

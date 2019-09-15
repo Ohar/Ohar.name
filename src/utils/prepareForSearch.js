b3173514974d8d67d70255b1f34a7ad5123c494f
Pavel Lysenko <code@ohar.name>
2019-09-16 00:04:47 +0300

    prepareForSearch: handle empty string and non-string inputs

--- src/utils/prepareForSearch.js
+++ src/utils/prepareForSearch.js
@@ -1,6 +1,9 @@
 module.exports = text => text
-  .toLowerCase()
-  .replace(/_/g, ' ')
-  .replace(/Ё/g, 'Е')
-  .replace(/ё/g, 'е')
-  .replace(/[.,:-]/g, '')
+  ? text
+    .toString()
+    .toLowerCase()
+    .replace(/_/g, ' ')
+    .replace(/Ё/g, 'Е')
+    .replace(/ё/g, 'е')
+    .replace(/[.,:-]/g, '')
+  : ''

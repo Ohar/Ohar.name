f31b4a139a7a3b90b632c4c9bf17bcca3b5f7f6e
Pavel Lysenko <code@ohar.name>
2019-10-06 12:43:36 +0200

    dndManualList: add english names

--- src/constants/dnd/dndManualList.js
+++ src/constants/dnd/dndManualList.js
@@ -4,34 +4,40 @@ const dndManualList = [
   {
     id: 'PHB',
     name: 'Книга игрока',
+    nameEn: 'Player’s Handbook',
     shortName: 'КИ',
   },
   {
     id: 'DMG',
     name: 'Руководство мастера',
+    nameEn: 'Dungeon Master’s Guide',
     shortName: 'РМ',
   },
   {
     id: 'MM',
     name: 'Бестиарий',
+    nameEn: 'Monster Manual',
     shortName: 'Б',
     includeMonsters: true,
   },
   {
     id: 'VGtM',
     name: 'Руководство Воло по монстрам',
+    nameEn: 'Volo’s Guide to Monsters',
     shortName: 'РВпМ',
     includeMonsters: true,
   },
   {
     id: 'ToA',
     name: 'Гробница аннигиляции',
+    nameEn: 'Tomb of Annihilation',
     shortName: 'ГА',
     includeMonsters: true,
   },
   {
     id: 'Homebrew',
     name: 'Самоделка',
+    nameEn: 'Homebrew',
     includeMonsters: true,
   },
 ]

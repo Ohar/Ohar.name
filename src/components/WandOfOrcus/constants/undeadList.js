963db149dbeaa8482600baca44e2bead4628ed51
Pavel Lysenko <code@ohar.name>
2019-10-09 17:27:57 +0200

    undeadList: clear code

--- src/components/WandOfOrcus/constants/undeadList.js
+++ src/components/WandOfOrcus/constants/undeadList.js
@@ -5,16 +5,9 @@ import calcDiceAverage from "@/utils/calcDiceAverage"
 
 import WAND_HP_LIMIT from "./WAND_HP_LIMIT"
 
-export default dndCreatureList
-  .filter(
-    ({creatureTypeIdList, hp}) => (
-      creatureTypeIdList.includes(CREATURE_TYPE_UNDEAD)
-      && calcDiceAverage(hp) <= WAND_HP_LIMIT
-    )
+export default dndCreatureList.filter(
+  ({creatureTypeIdList, hp}) => (
+    creatureTypeIdList.includes(CREATURE_TYPE_UNDEAD)
+    && calcDiceAverage(hp) <= WAND_HP_LIMIT
   )
-  // .map(
-  //   ({hp, id}) => ({
-  //     id,
-  //     hp: calcDiceAverage(hp),
-  //   })
-  // )
+)
